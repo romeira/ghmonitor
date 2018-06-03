4ecf2518552a284525244cc2022e31c7ac06bf9d
Paulo Romeira <paulo@pauloromeira.com>
2018-06-03 15:20:02 -0300

    Remove docker-compose and tweak run.sh

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.6
-
-COPY Pipfile .
-COPY Pipfile.lock .
-
-RUN pip install -U pipenv
-RUN pipenv install --dev --system
-
-WORKDIR /app
--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
-docker-compose up -d
+docker run -d --name ghmonitor_postgres \
+           -p 5555:5432 \
+           -v $PWD/db.postgres:/var/lib/postgresql/data \
+           -e POSTGRES_PASSWORD=ghmonitor \
+           -e POSTGRES_USER=ghmonitor \
+           postgres
+
+npm run start &
+
 pipenv run python manage.py runserver
-docker-compose down
+
+docker rm -f ghmonitor_postgres
